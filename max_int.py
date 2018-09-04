e74bd292e4760910695ad1f15735ef18b9a87f2f
Jón Bjarni Ólafsson <jon.bjarni.olafsson@advania.is>
2018-09-04 16:01:37 +0000

    functional program

--- max_int.py
+++ max_int.py
@@ -1,8 +1,11 @@
-
-num_int = int(input("Input a number: "))    # Do not change this line
 max_int = 0
-while num_int > 0:
-    print("123")
-    break
+while True:
+    num_int = int(input("Input a number: "))    # Do not change this line
+    if num_int < 0:
+        break
+
+    if num_int > max_int:
+        max_int = num_int
+
 
 print("The maximum is", max_int)    # Do not change this line
