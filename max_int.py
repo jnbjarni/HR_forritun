2701cad33a8a63e43f2fd94279f3bcb9003209b2
Jón Bjarni Ólafsson <jon.bjarni.olafsson@advania.is>
2018-09-04 15:35:45 +0000

    init commit

---
+++ max_int.py
@@ -0,0 +1,8 @@
+
+num_int = int(input("Input a number: "))    # Do not change this line
+max_int = 0
+while num_int > 0:
+    print("123")
+    break
+
+print("The maximum is", max_int)    # Do not change this line
